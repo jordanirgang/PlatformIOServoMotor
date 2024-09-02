92549620b7a4747acf3e45db1c0d1e502fa52b7e
birdman <jordanirgang@gmail.com>
2024-09-02 21:32:10 +0000

    added controller c file

---
+++ lib/Controller/controller.c
@@ -0,0 +1,19 @@
+extern "C" {
+	float integrateHistory =0;
+	float filterHistory=0;
+	float derivativeHistory =0; 
+
+	void FowardsEuler(float * signal, float timestep){
+
+	}
+
+	void LowPassFilter(float *signal, float alpha){
+
+	}
+
+	void BackwardEuler(float *signal, float alpha){
+
+	}
+
+
+}	
