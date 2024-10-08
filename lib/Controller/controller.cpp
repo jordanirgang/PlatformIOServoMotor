cdc9eaee8543302cdd282731081dc9086728bfa4
Jordan Irgang <jordanirgang@gmail.com>
2024-10-08 15:18:18 -0400

    saving container progress

--- lib/Controller/controller.cpp
+++ lib/Controller/controller.cpp
@@ -1,4 +1,6 @@
 #include "controller.h"
+//TODO: delete this
+#include <iostream>
 
 
 Generic::ErrorModifier::ErrorModifier(const float & gain)
@@ -38,15 +40,34 @@ void Generic::FowardEuler::operation(float*signalOut,float & error){
 
 void Generic::LowPassFilter::operation(float*signalOut,float & signalIn){
     // k is alpha measurement gain, signal in should be the error
-    *signalOut = (signalIn* this->k + (1-this->k)* this->history);
-    this-> history =  *signalOut;
+    //TODO: delete this
+    //std::cout << "Low pass Filter" <<signalIn<<"in:out"<<this->history<<"\n";//* this->k;
 
-}
+            //global filter_history
+            //filter_history = filter_history*alpha_gain + (1- alpha_gain) * update
+
+            //backup_history = filter_history
+            //filtered_update = LowPassFilter(update,alpha_gain)
+            //return (filtered_update-backup_history)/time_step
+
+        this-> history  = (signalIn) * this->k + (1-this->k)* this->history;
+
+        }
 
 void Generic::BackwardEuler::operation(float*signalOut,float & error){
-    this->LPF->operation(signalOut,error);
-    this-> history = this->LPF->GetHistory();
-    float update( (*signalOut - this->history)/this->timeStep);
+    //this-> history = this->LPF->GetHistory();
+
+    float backup_history(this->GetHistory());
+    //std::cout << "hostory"<< backup_history<< "\n";
+
+    //std::cout << "lpf in"<< this->LPF->GetHistory() << "\n";
+    this->LPF->operation(signalOut, error); //filtered update
+    
+    //std::cout << "lpf out"<< this->LPF->GetHistory()<< "\n";
+
+    float update( (this->LPF->GetHistory() - this->history)/this->timeStep);
+    std::cout << "Backward Euler" << *signalOut << " + " << this->k << "*" << update << "\n";
+    this-> history = update;
     *signalOut += this->k * update;
 }
 
--- test/test_control/test_control.cpp
+++ test/test_control/test_control.cpp
@@ -1,11 +1,12 @@
 #include "unity.h"
 #include "controller.h"
+#include <iostream>
 
 
-#define Kp .1
-#define Ki 10
-#define Kd 1
-#define Kalpha .65
+#define Kp .02
+#define Ki .02
+#define Kd .002
+#define Kalpha .998
 using namespace Generic;
 
 
@@ -21,7 +22,7 @@ void tearDown(){
 *ErrorModifier(const float & gain);
 *virtual void operation(float*signalOut,float & error){}
 *void UpdateError(float & error);
-*float GetHistory();
+*float GetHistory();	z	
 *void Step(float & timeStep);
 };
  *
@@ -46,6 +47,8 @@ float testController(ErrorModifier &control, const int & maxSteps,const float & 
 		diff = setPoint - *currPoint;
 		control.Step(.1); 
 		control.operation(currPoint,diff);
+		std::cout << *currPoint << "\n";
+
 	}
 	delete currPoint;
 	//debug only, comment out
@@ -63,12 +66,15 @@ void testIntegral(){
 }
 void testDerivative(){
 	int setPoint(100);
-	int diff = testController(derivative, 1000,setPoint);
+	int diff = testController(derivative,10,setPoint);
+	std::cout<< diff;
+	TEST_ASSERT_FLOAT_WITHIN(.5, 0, diff);
+
 	//TODO verify setpoint is correct
 }
 
 void testPID(){
-	int maxSteps(1000);
+	int maxSteps(80);
 	float setPoint(100);
 	float* currPoint= new float(0);
 	float diff;
@@ -77,13 +83,14 @@ void testPID(){
 	derivative.WipeHistory();
 	for(int i = 0; i< maxSteps;i++){
 		diff = setPoint -*currPoint;
-		integral.Step(.1);
-		porportion.Step(.1);
-	//	derivative.Step(.1);
+		integral.Step(1);
+		porportion.Step(1);
+		derivative.Step(1);
 
 		porportion.operation(currPoint,diff);
 		integral.operation(currPoint,diff);
-	//	derivative.operation(currPoint,diff);
+		derivative.operation(currPoint,diff);
+		std::cout << *currPoint << "\n";
 		
 	}
 	diff = setPoint-*currPoint;
@@ -95,9 +102,9 @@ void testPID(){
 
 int runUnityTests(void) {
 	  UNITY_BEGIN();
-	    RUN_TEST(testPorportion);
-	    RUN_TEST(testIntegral);
-	    RUN_TEST(testDerivative);
+	    //RUN_TEST(testPorportion);
+	    //RUN_TEST(testIntegral);
+	    //RUN_TEST(testDerivative);
 	    RUN_TEST(testPID);
 	      return UNITY_END();
 }
